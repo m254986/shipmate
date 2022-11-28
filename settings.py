068bb1f78275217965ce8fc5b689cf511a39d8c8
m254986 <115638575+m254986@users.noreply.github.com>
2022-11-27 22:23:48 -0500

    i fixed it :)
    next up: key up and key down events!

--- settings.py
+++ settings.py
@@ -5,7 +5,6 @@ class Settings:
         # Screen settings.
         self.screen_width = 128*5
         self.screen_height = 128*4
-        self.bg_color = (0, 0, 0)
 
         # van settings
         self.van_speed = 1.5
--- shipmate.py
+++ shipmate.py
@@ -3,42 +3,11 @@ import pygame
 
 from duty_van import Van
 from settings import Settings
+from interface import Interface
 
 # how to organize using classes and functions?
 clock = pygame.time.Clock()
 
-screen = pygame.display.set_mode((128 * 5, 128 * 4))
-road = pygame.image.load('images/road_asphalt22.png')
-road_rect = road.get_rect()
-
-left_road = pygame.image.load('images/road_asphalt21.png')
-right_road = pygame.image.load('images/road_asphalt23.png')
-grass = pygame.image.load('images/land_grass11.png')
-
-screen.blit(grass, (0,0))
-screen.blit(grass, (0, road_rect.height))
-screen.blit(grass, (0, road_rect.height*2))
-screen.blit(grass, (0, road_rect.height*3))
-
-screen.blit(left_road, (road_rect.width,0))
-screen.blit(left_road, (road_rect.width, road_rect.height))
-screen.blit(left_road, (road_rect.width, road_rect.height*2))
-screen.blit(left_road, (road_rect.width, road_rect.height*3))
-
-screen.blit(road, (road_rect.width*2,0))
-screen.blit(road, (road_rect.width*2,road_rect.height))
-screen.blit(road, (road_rect.width*2, road_rect.height*2))
-screen.blit(road, (road_rect.width*2, road_rect.height*3))
-
-screen.blit(right_road, (road_rect.width*3,0))
-screen.blit(right_road, (road_rect.width*3,road_rect.height))
-screen.blit(right_road, (road_rect.width*3, road_rect.height*2))
-screen.blit(right_road, (road_rect.width*3, road_rect.height*3))
-
-screen.blit(grass, (road_rect.width*4,0))
-screen.blit(grass, (road_rect.width*4, road_rect.height))
-screen.blit(grass, (road_rect.width*4, road_rect.height*2))
-screen.blit(grass, (road_rect.width*4, road_rect.height*3))
 class Shipmate:
     """Overall class to manage game assets and behavior."""
 
@@ -62,7 +31,39 @@ class Shipmate:
             # 2. update game objects
             self.van.update()
             # 3. draw screen
-            self.screen.fill(self.settings.bg_color)
+            screen = pygame.display.set_mode((128 * 5, 128 * 4))
+
+            road = pygame.image.load('images/road_asphalt22.png')
+            road_rect = road.get_rect()
+
+            left_road = pygame.image.load('images/road_asphalt21.png')
+            right_road = pygame.image.load('images/road_asphalt23.png')
+            grass = pygame.image.load('images/land_grass11.png')
+
+            screen.blit(grass, (0, 0))
+            screen.blit(grass, (0, road_rect.height))
+            screen.blit(grass, (0, road_rect.height * 2))
+            screen.blit(grass, (0, road_rect.height * 3))
+
+            screen.blit(left_road, (road_rect.width, 0))
+            screen.blit(left_road, (road_rect.width, road_rect.height))
+            screen.blit(left_road, (road_rect.width, road_rect.height * 2))
+            screen.blit(left_road, (road_rect.width, road_rect.height * 3))
+
+            screen.blit(road, (road_rect.width * 2, 0))
+            screen.blit(road, (road_rect.width * 2, road_rect.height))
+            screen.blit(road, (road_rect.width * 2, road_rect.height * 2))
+            screen.blit(road, (road_rect.width * 2, road_rect.height * 3))
+
+            screen.blit(right_road, (road_rect.width * 3, 0))
+            screen.blit(right_road, (road_rect.width * 3, road_rect.height))
+            screen.blit(right_road, (road_rect.width * 3, road_rect.height * 2))
+            screen.blit(right_road, (road_rect.width * 3, road_rect.height * 3))
+
+            screen.blit(grass, (road_rect.width * 4, 0))
+            screen.blit(grass, (road_rect.width * 4, road_rect.height))
+            screen.blit(grass, (road_rect.width * 4, road_rect.height * 2))
+            screen.blit(grass, (road_rect.width * 4, road_rect.height * 3))
             self.van.blitme()
             pygame.display.flip()
             # 4. win/lose conditions
